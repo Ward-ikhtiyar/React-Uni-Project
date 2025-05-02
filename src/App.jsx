8a5644d0bf0e5f79e94990f8eaca82d7b8bc2ce4
Zain00F <clasheyad@gmail.com>
2025-05-02 17:35:10 +0300

    Refactoring Display Card ( Temporary info json file instead of props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+import DisplayPage from './Presentation/Pages/Display/Pages/Display_Page';
 import ShowHouse from './Presentation/Pages/ShowPages/Page/show_house';
 import HomePage from './Presentation/Pages/Home/Pages/HomePage';
 import Login_page from './Presentation/Pages/Auth/Auth_page';
@@ -8,19 +9,9 @@ function App() {
 
   return (
     <>
-    <div className='property-card'>
-      
-      <div className='property-pic'>
-      </div>
-      <div className='price-favorite'>2,300$
-      </div>
-      <div className='info'>4 beds<hr></hr>2 ba<hr></hr>1,535 sqft <hr></hr> House for sale
-      </div>
-      <div className='info'>Damascus,Qudsaya
-      </div>
-    </div>
     {/* <HomePage/> */}
       {/* <ShowHouse/> */}
+    <DisplayPage/>
     </>
   )
 }
---
+++ src/Presentation/Pages/Display/Pages/Display_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid, Box } from '@mui/material';
+import Card from "../components/Display_Card"
+const DisplayPage = () => {
+    return (
+        <Card
+        />
+    );
+}
+
+export default DisplayPage;
