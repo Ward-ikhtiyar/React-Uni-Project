592d7c01723ef509e98e1d9902ec8a01dccdd267
Zain00F <clasheyad@gmail.com>
2025-06-21 05:22:23 +0300

    edited the footer, and routed the checkout out button on hero image

--- src/Presentation/Pages/Home/components/adSections.jsx
+++ src/Presentation/Pages/Home/components/adSections.jsx
@@ -1,14 +1,17 @@
 import './adSection.css'
 import AppBar from './appBar';
+import { useNavigate } from 'react-router-dom';
 function AdSection(){
-    
+    let navigate = useNavigate();
     return(
         <div className="Main-div">
             <AppBar isHome={true} />
             <div className='grid'>
                  <div className='button-section' >
                 <div className='CTA' style={{color: 'white'}}>Homes.Lands<br></br>Apartments.</div>
-                <button className='checkout-button'>Check out</button>
+                <button className='checkout-button' onClick={() => {
+                    navigate('/Properties')
+                }}>Check out</button>
             </div>
             <div className='alt-logo'  ><a href='https://google.com'>SpeedOrder</a></div>
             </div>
